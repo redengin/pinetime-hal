8b08693c8c616264914bb25c9a2a56311d3151f2
redengin <sholstei@yahoo.com>
2022-05-08 01:05:03 -0400

    hoping this prints HelloWorld

--- examples/rtic_demo.rs
+++ examples/rtic_demo.rs
@@ -1,40 +1,75 @@
 #![no_std]
 #![no_main]
 
-use nrf52832_hal as _;
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-use pinetime_hal::monotonic_nrf52::{MonoTimer};
 
 
 #[rtic::app(device = nrf52832_hal::pac, peripherals = true, dispatchers = [SWI0_EGU0])]
 mod app {
-    use super::*;
+    use nrf52832_hal as _;
+    use rtt_target::{rprintln, rtt_init_print};
+    use pinetime_hal::monotonic_nrf52::{MonoTimer};
+    use fugit::{self, ExtU32};
+    use pinetime_hal::Pinetime;
 
-    #[shared]
-    struct Shared {}
-
-    #[local]
-    struct Local {}
+    use embedded_graphics::{
+        prelude::*,
+        pixelcolor::Rgb565,
+        text::Text,
+        mono_font::{ascii::FONT_6X9, MonoTextStyle},
+    };
 
     #[monotonic(binds = TIMER1, default = true)]
     type Tonic = MonoTimer<nrf52832_hal::pac::TIMER1>;
 
+    #[shared]
+    struct Shared {
+        pinetime: pinetime_hal::Pinetime,
+    }
+
+
+    #[local]
+    struct Local {
+    }
+
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // initialize scheduler timer
         let mono = MonoTimer::new(cx.device.TIMER1);
+
+        let pinetime = Pinetime::init(
+            cx.device.TIMER0,
+            cx.device.P0,
+            cx.device.SAADC,
+            cx.device.SPIM1,
+            cx.device.TWIM1,
+        );
 
         rtt_init_print!();
         rprintln!("init");
 
-        task1::spawn().ok();
+        display_task::spawn().ok();
 
-        (Shared {}, Local {}, init::Monotonics(mono))
+        ( Shared {
+            pinetime,
+          },
+          Local {
+          },
+          init::Monotonics(mono)
+        )
     }
 
-    #[task]
-    fn task1(_cx: task1::Context) {
-        rprintln!("task1");
-        // task1::spawn_after(2000.millis()).ok();
+    #[task(shared=[pinetime])]
+    fn display_task(cx: display_task::Context) {
+        // run at 30Hz
+        display_task::spawn_after(33.millis()).unwrap();
+
+        let mut pinetime = cx.shared.pinetime;
+        (pinetime).lock(|pinetime| {
+            let text_style = MonoTextStyle::new(&FONT_6X9, Rgb565::GREEN);
+            Text::new("Hello World!", Point::new(0,0), text_style)
+                .draw(&mut pinetime.screen)
+                .unwrap();
+        });
     }
 }
